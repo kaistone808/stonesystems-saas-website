9c7b0edea63ff59e21eeb017449de1663a658533
Michael Vanhoutte <michael3vanhoutte@gmail.com>
2024-08-20 18:58:53 +0200

    make pwa and update head

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,35 @@
-import bundleAnalyzer from '@next/bundle-analyzer';
+import {
+  PHASE_DEVELOPMENT_SERVER,
+  PHASE_PRODUCTION_BUILD,
+} from "next/constants.js";
+import bundleAnalyzer from "@next/bundle-analyzer";
 
-const withBundleAnalyzer = bundleAnalyzer({
-  enabled: process.env.ANALYZE === 'true',
-});
-
-export default withBundleAnalyzer({
-  reactStrictMode: false,
+/** @type {import("next").NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
   eslint: {
     ignoreDuringBuilds: true,
   },
   experimental: {
-    optimizePackageImports: ['@mantine/core', '@mantine/hooks'],
+    optimizePackageImports: ["@mantine/core", "@mantine/hooks"],
   },
+};
+
+const withBundleAnalyzer = bundleAnalyzer({
+  enabled: process.env.ANALYZE === "true",
 });
+
+const nextConfigFunction = async (phase) => {
+  let config = nextConfig;
+
+  if (phase === PHASE_DEVELOPMENT_SERVER || phase === PHASE_PRODUCTION_BUILD) {
+    const withPWA = (await import("@ducanh2912/next-pwa")).default({
+      dest: "public",
+    });
+    config = withPWA(config);
+  }
+
+  return withBundleAnalyzer(config);
+};
+
+export default nextConfigFunction;
