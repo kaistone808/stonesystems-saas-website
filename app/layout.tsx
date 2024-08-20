c92f138f6401e682f9b62a6bacefe7aa19ced746
Michael Vanhoutte <michael3vanhoutte@gmail.com>
2024-08-20 19:01:53 +0200

    make pwa and update head

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,11 +2,52 @@ import '@mantine/core/styles.css';
 import React from 'react';
 import { MantineProvider } from '@mantine/core';
 import { Sora } from 'next/font/google';
+import type { Metadata, Viewport } from 'next';
 import { theme } from '../theme';
 
 import '@mantine/carousel/styles.css';
 import '../globals.css';
 import { LayoutWrapper } from '@/components/LayoutWrapper/LayoutWrapper';
+
+const APP_NAME = 'Stone Systems';
+const APP_DEFAULT_TITLE = 'Stone Systems - Website Design & Marketing Systems For Contractors!';
+const APP_TITLE_TEMPLATE = 'Stone Systems - Website Design & Marketing Systems For Contractors!';
+const APP_DESCRIPTION = 'Contractors trust us to transform their business with marketing systems, website design, reputation management and automation. And we do all of that at an affordable price!';
+
+export const metadata: Metadata = {
+  applicationName: APP_NAME,
+  manifest: '/manifest.json',
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: 'default',
+    title: APP_DEFAULT_TITLE,
+    // startUpImage: [],
+  },
+  formatDetection: {
+    telephone: false,
+  },
+  openGraph: {
+    type: 'website',
+    siteName: APP_NAME,
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
+  twitter: {
+    card: 'summary',
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
+};
+
+export const viewport: Viewport = {
+  themeColor: '#e8a743',
+};
 
 const sora = Sora({
   subsets: ['latin'],
