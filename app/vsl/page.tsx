685ff9c2605850cb946b01485433b76b6d997b9e
Michael Vanhoutte <michael3vanhoutte@gmail.com>
2024-09-12 12:41:26 -1000

    add vsl page

---
+++ app/vsl/page.tsx
@@ -0,0 +1,18 @@
+import { Metadata } from 'next';
+import { TestimonialSection } from '@/components/VslPage/TestimonialSection/TestimonialSection';
+import { IntroSection } from '@/components/VslPage/IntroSection/IntroSection';
+
+export const metadata: Metadata = {
+  title: 'Ezclick - VSL',
+  description:
+    'Contractors trust us to transform their business with marketing systems, website design, reputation management and automation. And we do all of that at an affordable price!',
+};
+
+export default function VSLPage() {
+  return (
+    <>
+      <IntroSection />
+      <TestimonialSection />{' '}
+    </>
+  );
+}
---
+++ components/VslPage/IntroSection/IntroSection.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import { Container, Title } from '@mantine/core';
+import classes from './IntroSection.module.css';
+import LazyLoadVideo from '@/components/LazyLoadVideo/LazyLoadVideo';
+
+export function IntroSection() {
+  return (
+    <div className={classes.outer}>
+      <Container className={classes.card} size="xl">
+        <Title className={classes.mainTitle} order={1}>
+          How Frank Added $20,000 of Profit To His Business While Paying Us $197/month
+        </Title>
+        <LazyLoadVideo
+          poster={'/images/thumbnails/vsl.jpeg'}
+          alt={'vsl'}
+          src={'/videos/vsl.mp4'}
+          type="video/mp4"
+          width="100%"
+          height="auto"
+        />
+      </Container>
+    </div>
+  );
+}
