4c400e638c7481e8ebfc7fd18d0a95187a9e06e5
Michael Vanhoutte <michael3vanhoutte@gmail.com>
2025-04-03 16:39:29 -0500

    contact page update

--- components/ContactUsPage/IntroSection/IntroSection.tsx
+++ components/ContactUsPage/IntroSection/IntroSection.tsx
@@ -20,6 +20,24 @@ export function IntroSection() {
           ></iframe>
           <script src="https://api.ezclick.io/js/form_embed.js" type="text/javascript"></script>
         </Container>
+        <iframe
+          src="https://links.stonesystems.io/widget/form/wsgeBOWeKk5cflebd4to"
+          style={{ width: '100%', height: '100%', border: 'none', borderRadius: '3px' }}
+          id="inline-wsgeBOWeKk5cflebd4to"
+          data-layout="{'id':'INLINE'}"
+          data-trigger-type="alwaysShow"
+          data-trigger-value=""
+          data-activation-type="alwaysActivated"
+          data-activation-value=""
+          data-deactivation-type="neverDeactivate"
+          data-deactivation-value=""
+          data-form-name="Opt-In Form"
+          data-height="656"
+          data-layout-iframe-id="inline-wsgeBOWeKk5cflebd4to"
+          data-form-id="wsgeBOWeKk5cflebd4to"
+          title="Opt-In Form"
+        ></iframe>
+        <script src="https://links.stonesystems.io/js/form_embed.js"></script>
       </Container>
     </div>
   );
