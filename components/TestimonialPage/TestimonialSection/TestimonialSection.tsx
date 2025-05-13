8c73b8f6d122c2dd911fba863772435a7f524376
Michael Vanhoutte <michael3vanhoutte@gmail.com>
2025-05-13 14:24:00 -0500

    update testimonials

--- components/TestimonialPage/TestimonialSection/TestimonialSection.tsx
+++ components/TestimonialPage/TestimonialSection/TestimonialSection.tsx
@@ -41,6 +41,27 @@ function Card({ personName, testimonial, videoSrc, videoThumbnail }: CardProps) 
 const data = [
   {
     testimonial:
+      `"Highly recommended, they are here to help us contractors that don't like to deal with the website stuff. It feels like you have partner looking out for you that's actually helping you succeed."`,
+    videoSrc: '/videos/Adam1.mp4#t=0.1',
+    videoThumbnail: '/images/thumbnails/Adam1.png',
+    personName: '-Adam',
+  },
+  {
+    testimonial:
+      `"I've been working with stonesystems for the last few months and it's been absolutely amazing. If you're out there and you need someone that's gonna be there for you... we love working with stonesystems"`,
+    videoSrc: '/videos/Dale.mp4#t=0.1',
+    videoThumbnail: '/images/thumbnails/Dale.png',
+    personName: '-Dale',
+  },
+  {
+    testimonial:
+      `"Shoutout to Kai and Remy for an amazing job that they did doing my website. I've been a pain in the butt every now and then but they did it right. Check them out, you won't be dissapointed."`,
+    videoSrc: '/videos/George.mp4#t=0.1',
+    videoThumbnail: '/images/thumbnails/George.png',
+    personName: '-George',
+  },
+  {
+    testimonial:
     '"I would highly recommend going with Stone Systems, my business has ramped up, literally overnight and their prices are very affordable. Feel free to reach out to me personally with any questions about them, thank you!"',
     videoSrc: '/videos/Frank.mp4#t=0.1',
     videoThumbnail: '/images/thumbnails/Frank.jpg',
