07e79dccffbf89427cfc744285fb113674f76c64
Michael Vanhoutte <michael3vanhoutte@gmail.com>
2025-05-16 20:32:33 -0500

    test to see contact page fix

--- components/ContactUsPage/IntroSection/IntroSection.tsx
+++ components/ContactUsPage/IntroSection/IntroSection.tsx
@@ -1,12 +1,9 @@
 'use client';
 
-import { Container, Title, useMantineTheme } from '@mantine/core';
+import { Container, Title } from '@mantine/core';
 import classes from './IntroSection.module.css';
-import { useMediaQuery } from '@mantine/hooks';
 
 export function IntroSection() {
-  const theme = useMantineTheme();
-  const mobile = useMediaQuery(`(max-width: ${theme.breakpoints.sm})`);
   return (
     <div className={classes.outer}>
       <Container size="lg">
@@ -14,11 +11,11 @@ export function IntroSection() {
         <Container className={classes.calendarSection} size="lg">
           <iframe
             src="https://link.stonesystems.io/widget/booking/4APjYC2WFZGvExeA9wq6"
-            style={{ width: '100%', border: 'none', overflow: 'hidden' }}
+            style={{ width: '100%', height: '800px', border: 'none', overflow: 'hidden' }}
             scrolling="no"
-            id="GETOH8LH2gfqjMI5Y5kX_1746308399715"
+            id="calendar-iframe"
+            loading="lazy"
           ></iframe>
-          <script src="https://links.stonesystems.io/js/form_embed.js" type="text/javascript"></script>
         </Container>
       </Container>
     </div>
